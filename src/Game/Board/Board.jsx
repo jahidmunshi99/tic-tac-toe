705e9fc8344d60949e753b682ff909221d2edcec
Jahid Munshi <jahidmunshi99@gmail.com>
2025-05-29 13:10:43 +0600

    diclared winner

--- src/Game/Board/Board.jsx
+++ src/Game/Board/Board.jsx
@@ -3,13 +3,25 @@ import Squore from "./Squore";
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
-
   const [xIsNext, setXIsNext] = useState(true);
+
+  {
+    /** Diclare The winner */
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner : ${winner}`;
+  } else {
+    status = `Next Player: ${xIsNext ? "X" : "O"}`;
+  }
 
   function handleClick(index) {
     const nextSqares = squares.slice();
 
-    if (squares[index]) {
+    if (squares[index] || winner) {
       return;
     }
 
@@ -30,6 +42,7 @@ const Board = () => {
         <p className="text-center">
           This is where the game board will be displayed.
         </p>
+        {status}
         <div className="game-board flex flex-col items-center justify-center mt-10">
           {/* Game board will be rendered here */}
           <div className="flex">
@@ -54,3 +67,23 @@ const Board = () => {
 };
 
 export default Board;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
